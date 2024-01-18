b746a1b42694cf479c8c9e6a1bfc372191f190a4
Sorbopriyo Roy <sorbopriyo@gmail.com>
2024-01-19 03:20:47 +0530

    map showing

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.js";
+import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css";
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 const MapView = () => {
-  return <div>MapView</div>;
+  const map = useRef(null);
+
+  useEffect(() => {
+    // initialize map only once
+    if (map.current) return;
+    map.current = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [0, 0],
+      zoom: 15,
+      attributionControl: false,
+    });
+  }, []);
+
+  return (
+    <main>
+      <div id="map"></div>
+    </main>
+  );
 };
 
 export default MapView;
